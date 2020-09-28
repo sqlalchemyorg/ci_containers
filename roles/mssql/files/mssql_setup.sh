dd17a021dcb9e3726e3e17f54aeb7fc71da4a0c7
Mike Bayer <mike_mp@zzzcomputing.com>
2020-09-28 15:25:39 -0400

    add mssql to the "fast" build

--- roles/mssql/files/mssql_setup.sh
+++ roles/mssql/files/mssql_setup.sh
@@ -8,7 +8,7 @@ export MSSQL_PID=Developer
 
 /opt/mssql/bin/sqlservr & PID=$!
 
-sleep 10s
+sleep 30s
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${MSSQL_SA_PASSWORD}" -d master -i /mssql_setup.sql
 
