61d0e48b845793fb1eb22dbd11cd64099fd122b6
Mike Bayer <mike_mp@zzzcomputing.com>
2021-05-26 16:50:05 -0400

    python 3.10 updates
    - add python 3.10.0b1
    - remove python 3.5 as pip doesnt have a standard get-pip for it anymore
    - for python 2.7, we use a legacy get-pip

--- roles/jenkins/files/install_cpython.sh
+++ roles/jenkins/files/install_cpython.sh
@@ -5,63 +5,63 @@ set -e
 
 function install_python() {
 
-	VERSION=$1
-	SIMPLE_NUMBER=`echo ${VERSION} | awk -F. '{ print $1"."$2 }'`
-	MAJOR_VERSION=`echo ${VERSION} | awk -F. '{ print $1 }'`
-	NON_BETA_VERSION=`echo ${VERSION} | sed -r 's/([0-9\.]+).*?/\1/'`
+    VERSION=$1
+    SIMPLE_NUMBER=`echo ${VERSION} | awk -F. '{ print $1"."$2 }'`
+    MAJOR_VERSION=`echo ${VERSION} | awk -F. '{ print $1 }'`
+    NON_BETA_VERSION=`echo ${VERSION} | sed -r 's/([0-9\.]+).*?/\1/'`
 
-	echo "VERSION=${VERSION} SIMPLE_NUMBER=${SIMPLE_NUMBER} MAJOR_VERSION=${MAJOR_VERSION} NON_BETA_VERSION=${NON_BETA_VERSION}"
+    echo "VERSION=${VERSION} SIMPLE_NUMBER=${SIMPLE_NUMBER} MAJOR_VERSION=${MAJOR_VERSION} NON_BETA_VERSION=${NON_BETA_VERSION}"
 
-	if [[ -d /opt/python-${VERSION} ]]; then
-		echo "Version ${VERSION} is already present, not rebuilding"
-		return
-	fi
-
-
-	if [[ "${MAJOR_VERSION}" == 3 ]]; then
-		PYTHON_INTERP_NAME="python3"
-		PIP_NAME="pip3"
-		CONFIGURE_ARGS=""
-	else
-		PYTHON_INTERP_NAME="python"
-		PIP_NAME="pip"
-
-		# pylibmc has wheel files
-		# the one for 2.7 is manylinux-mu only
-		# set ucs4 so that an "mu" wheel can install,
-		# see https://github.com/pypa/manylinux
-		# centos8 has no libmemcached-devel, but there's
-		# no pylibmc wheel for python 3.8 anyway so we are
-		# still having to build libmemcached to get the header files
-		CONFIGURE_ARGS="--enable-unicode=ucs4"
-	fi
-
-	cd /usr/local/src
-
-	# rm -fr Python-${VERSION}
-
-	if [ ! -f "Python-${VERSION}.tar.xz" ]; then
-		curl -L -O https://www.python.org/ftp/python/${NON_BETA_VERSION}/Python-${VERSION}.tar.xz
-	fi
-
-	if [ ! -d "/opt/python-${VERSION}" ]; then
-		ls -la
-		tar -xf Python-${VERSION}.tar.xz
-		cd Python-${VERSION}
-		./configure --prefix /opt/python-${VERSION} ${CONFIGURE_ARGS}
-		make
-		make install
-
-		rm -f /opt/python${SIMPLE_NUMBER}
-		ln -s /opt/python-${VERSION} /opt/python${SIMPLE_NUMBER}
-	fi
-
-	/opt/python${SIMPLE_NUMBER}/bin/${PYTHON_INTERP_NAME} /usr/local/src/get-pip.py
+    if [[ -d /opt/python-${VERSION} ]]; then
+        echo "Version ${VERSION} is already present, not rebuilding"
+        return
+    fi
 
 
-	/opt/python${SIMPLE_NUMBER}/bin/${PIP_NAME} install pip tox coverage flake8-junit-report --upgrade
+    if [[ "${MAJOR_VERSION}" == 3 ]]; then
+        PYTHON_INTERP_NAME="python3"
+        PIP_NAME="pip3"
+        GET_PIP="get-pip.py"
+        CONFIGURE_ARGS=""
+    else
+        PYTHON_INTERP_NAME="python"
+        PIP_NAME="pip"
+        GET_PIP="get-pip-27.py"
+        # pylibmc has wheel files
+        # the one for 2.7 is manylinux-mu only
+        # set ucs4 so that an "mu" wheel can install,
+        # see https://github.com/pypa/manylinux
+        # centos8 has no libmemcached-devel, but there's
+        # no pylibmc wheel for python 3.8 anyway so we are
+        # still having to build libmemcached to get the header files
+        CONFIGURE_ARGS="--enable-unicode=ucs4"
+    fi
+
+    cd /usr/local/src
+
+    # rm -fr Python-${VERSION}
+
+    if [ ! -f "Python-${VERSION}.tar.xz" ]; then
+        curl -L -O https://www.python.org/ftp/python/${NON_BETA_VERSION}/Python-${VERSION}.tar.xz
+    fi
+
+    if [ ! -d "/opt/python-${VERSION}" ]; then
+        ls -la
+        tar -xf Python-${VERSION}.tar.xz
+        cd Python-${VERSION}
+        ./configure --prefix /opt/python-${VERSION} ${CONFIGURE_ARGS}
+        make
+        make install
+
+        rm -f /opt/python${SIMPLE_NUMBER}
+        ln -s /opt/python-${VERSION} /opt/python${SIMPLE_NUMBER}
+    fi
+
+    /opt/python${SIMPLE_NUMBER}/bin/${PYTHON_INTERP_NAME} /usr/local/src/${GET_PIP}
+
+    /opt/python${SIMPLE_NUMBER}/bin/${PIP_NAME} install pip tox coverage flake8-junit-report --upgrade
 }
 
 for python_version in $@; do
-	install_python ${python_version}
+    install_python ${python_version}
 done
