c35ed93c7e03b0cfb54d819c7d0c1839b485c25f
Mike Bayer <mike_mp@zzzcomputing.com>
2022-02-26 13:00:59 -0500

    add workflow to delete and re-launch all DB containers
    as DBs running for weeks will accumulate un-dropped databases
    from failed runs and possibly other artifacts, things can
    get chunky after awhile.  provide a one liner that
    will docker rm -f all database containers and re-start them
    fresh from the images.

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# rebuild containers and images
 # --extra-vars force_container_rebuild=true
+
+# delete containers with docker rm -f
+# --extra-vars delete_containers=true, --tags oracle
+
+# delete and relaunch all database containers from existing
+# images
+# ./run.sh --tags databases --extra-vars delete_containers=true
+
 
 ansible-playbook \
 	-vv -i hosts \
