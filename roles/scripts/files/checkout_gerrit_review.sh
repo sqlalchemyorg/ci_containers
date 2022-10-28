723897d629aef2a3065f49fcbea98523830d6fb7
Mike Bayer <mike_mp@zzzcomputing.com>
2022-10-28 00:05:00 -0400

    more fixes
    includes a new script to do the gerrit checkout.  the git/gerrit
    integration in jenkins is working very poorly now, after some
    configuration fights, it's still hanging for no clear reason.
    direct cmdline checkout seems to work very well however.

---
+++ roles/scripts/files/checkout_gerrit_review.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# generic gerrit checkout script to run on jenkins
+
+# after many years of fighting with the jenkins git plugin and how it
+# interacts with gerrit, performing slow queries, hanging, being very
+# difficult to configure, demanding constant SSH key format changes, and just
+# being completely opaque, I realized that a lot of our jobs just do the
+# git checkout right in the script, using local ssh keys.   totally simple.
+# let's just do that.
+
+set -x
+
+export GERRIT_BASE=https://gerrit.sqlalchemy.org/
+
+if [[ ! -d ${GERRIT_PROJECT} ]]; then
+    git clone ${GERRIT_BASE}${GERRIT_PROJECT}.git ${GERRIT_PROJECT}
+fi
+
+cd ${GERRIT_PROJECT}
+git fetch ${GERRIT_BASE}${GERRIT_PROJECT} ${GERRIT_REFSPEC}
+git checkout FETCH_HEAD
