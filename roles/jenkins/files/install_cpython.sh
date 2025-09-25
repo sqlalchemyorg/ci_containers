0c77a8571f0df74d3e7e5ed07e5130e111144f8c
Mike Bayer <mike_mp@zzzcomputing.com>
2025-09-24 23:47:54 -0400

    add support for threaded
    this mostly works but I havent gotten the whole ansible to work.
    There may be some collisions with same-numbered versions happening,
    this version might work but too late to test it.

--- roles/jenkins/files/install_cpython.sh
+++ roles/jenkins/files/install_cpython.sh
@@ -5,15 +5,27 @@ set -e
 
 function install_python() {
 
-    VERSION=$1
-    SIMPLE_NUMBER=`echo ${VERSION} | awk -F. '{ print $1"."$2 }'`
+    RAWVERSION=$1
+
+    # search and grep out "optimize"; an "o" at the end
+    # of the version
+    OPTIMIZE=`echo ${RAWVERSION} | sed -n 's/.*o$/o/p'`
+    VERSION=`echo ${RAWVERSION} | sed 's/o$//'`
+
+    # search and grep out "threaded"; a "t" at the end
+    # of the version
+    THREADED=`echo ${VERSION} | sed -n 's/.*t$/t/p'`
+    DLVERSION=`echo ${VERSION} | sed 's/t$//'`
+
+    REALLY_SIMPLE_NUMBER=`echo ${VERSION} | awk -F. '{ print $1"."$2 }'`
+    SIMPLE_NUMBER="${REALLY_SIMPLE_NUMBER}${THREADED}"
     NODOTS_SIMPLE_NUMBER=`echo ${VERSION} | awk -F. '{ print $1$2 }'`
     MAJOR_VERSION=`echo ${VERSION} | awk -F. '{ print $1 }'`
     NON_BETA_VERSION=`echo ${VERSION} | sed -r 's/([0-9\.]+).*?/\1/'`
 
     cd /usr/local/src
 
-    echo "VERSION=${VERSION} SIMPLE_NUMBER=${SIMPLE_NUMBER} MAJOR_VERSION=${MAJOR_VERSION} NON_BETA_VERSION=${NON_BETA_VERSION}"
+    echo "VERSION=${VERSION} SIMPLE_NUMBER=${SIMPLE_NUMBER} MAJOR_VERSION=${MAJOR_VERSION} NON_BETA_VERSION=${NON_BETA_VERSION} OPTIMIZE=${OPTIMIZE} THREADED=${THREADED}"
 
     if [[ -d /opt/python-${VERSION} ]]; then
         echo "Version ${VERSION} is already present, not rebuilding"
@@ -24,7 +36,7 @@ function install_python() {
     # generic get-pip stops working as soon as a version is EOL
     # however, they dont put the per-version one up until it's EOL.
     # so --fail means it wont write a file if it's 404
-    curl --fail -L https://bootstrap.pypa.io/pip/${SIMPLE_NUMBER}/get-pip.py  -o get-pip-${NODOTS_SIMPLE_NUMBER}.py || true
+    curl --fail -L https://bootstrap.pypa.io/pip/${REALLY_SIMPLE_NUMBER}/get-pip.py  -o get-pip-${NODOTS_SIMPLE_NUMBER}.py || true
 
     if [[ -f get-pip-${NODOTS_SIMPLE_NUMBER}.py ]]; then
         GET_PIP=get-pip-${NODOTS_SIMPLE_NUMBER}.py
@@ -37,7 +49,7 @@ function install_python() {
         PYTHON_INTERP_NAME="python3"
         PIP_NAME="pip3"
 
-        if [[ -n "${ENABLE_OPTIMIZATIONS}" ]]; then
+        if [[ "${OPTIMIZE}" != "" ]]; then
             CONFIGURE_ARGS="--enable-optimizations"
         else
             CONFIGURE_ARGS="--with-lto"
@@ -55,17 +67,22 @@ function install_python() {
         CONFIGURE_ARGS="--enable-unicode=ucs4"
     fi
 
+    if [[ "${THREADED}" != "" ]]; then
+        CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-gil"
+    fi
 
     # rm -fr Python-${VERSION}
 
-    if [ ! -f "Python-${VERSION}.tar.xz" ]; then
-        curl -L -O https://www.python.org/ftp/python/${NON_BETA_VERSION}/Python-${VERSION}.tar.xz
+    if [ ! -f "Python-${DLVERSION}.tar.xz" ]; then
+        curl -L -O https://www.python.org/ftp/python/${NON_BETA_VERSION}/Python-${DLVERSION}.tar.xz
     fi
 
-    if [ ! -d "/opt/python-${VERSION}" ]; then
-        ls -la
-        tar -xf Python-${VERSION}.tar.xz
-        cd Python-${VERSION}
+    if [ ! -d "/opt/python-${RAWVERSION}" ]; then
+
+        # untar to folder matching the build we're going to do
+        mkdir -p "./Python-${RAWVERSION}"
+        tar -xf Python-${DLVERSION}.tar.xz -C "./Python-${RAWVERSION}" --strip-components=1
+        cd Python-${RAWVERSION}
         ./configure --prefix /opt/python-${VERSION} ${CONFIGURE_ARGS}
         make
         make install
